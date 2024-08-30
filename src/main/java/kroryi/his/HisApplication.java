2fbf73cc0b50aa7b1b02b97c936df7f37e1c6f82
이재준 <ksol0048@naver.com>
2024-08-30 18:52:40 +0900

    기본 layout 설정

--- src/main/java/kroryi/his/HisApplication.java
+++ src/main/java/kroryi/his/HisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HisApplication.class, args);
+    }
 
 }
--- src/main/java/kroryi/his/HomeController.java
+++ src/main/java/kroryi/his/HomeController.java
@@ -1,4 +1,51 @@
 package kroryi.his;
 
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
 public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "admin_management";
+    }
+
+//    사용자관리
+    @GetMapping("/admin_management")
+    public String admin_management() {
+        return "admin_management";
+    }
+
+//    환자등록
+    @GetMapping("/patient_register")
+    public String patient_register() {
+        return "patient_register";
+    }
+
+//    진료예약
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+//    진료차트
+    @GetMapping("/medical_chart")
+    public String medical_chart() {
+        return "medical_chart";
+    }
+
+//    진료접수
+    @GetMapping("/reception")
+    public String reception() {
+        return "reception";
+    }
+
+//    재고관리
+    @GetMapping("/inventory_management")
+    public String inventory_management() {
+        return "inventory_management";
+    }
 }
