74883b4ead0ad66f67c50ff3aa3d1d69151bfde5
cat <ideale_0@naver.com>
2024-09-19 22:42:57 +0900

    많은 일이 있었지만 해결했어요..?

--- src/main/java/kroryi/his/domain/MaterialRegister.java
+++ src/main/java/kroryi/his/domain/MaterialRegister.java
@@ -1,5 +1,6 @@
 package kroryi.his.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,6 +22,7 @@ public class MaterialRegister {
 
     //company의 업체코드와 join
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "company_Code")
     private CompanyRegister companyRegister;
 
