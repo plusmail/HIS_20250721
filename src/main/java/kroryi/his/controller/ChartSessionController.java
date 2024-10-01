15ab022ff21aefd17ea3ab8a87fa7de5c0476a28
Lania <tpdl7985@naver.com>
2024-10-01 10:26:27 +0900

    치아 컨트롤러 구현, 자주 사용하는 문구 구현 및 메모로 전송.

--- src/main/java/kroryi/his/controller/ChartSessionController.java
+++ src/main/java/kroryi/his/controller/ChartSessionController.java
@@ -1,11 +1,14 @@
 package kroryi.his.controller;
 
 import jakarta.servlet.http.HttpSession;
+import kroryi.his.dto.RequestData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -13,21 +16,44 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class ChartSessionController {
-    private HttpSession session;
-    // 세션에서 "nestedList" 가져오기
 
 
+    @PostMapping(value = "/his/adds-to-sublist")
+    private ResponseEntity<?> addsToSubList(@RequestBody RequestData data, HttpSession session) {
+        List<String> newValue = data.getNewValue();
+        int subListIndex = data.getSubListIndex();
 
+        List<List<String>> nestedList = getSessionItems(session);
+        List<String> innerList = nestedList.get(subListIndex);
+        for (int i = 0; i < newValue.size(); i++) {
+            boolean check = (innerList).contains(newValue.get(i));
+            if (check) {
+                i++;
+            } else {
+                addToSubList(subListIndex, newValue.get(i), session);
+            }
+        }
+        return ResponseEntity.ok(newValue);
+    }
+
+    @PostMapping(value = "/his/deletes-to-sublist", consumes = "application/json")
+    private ResponseEntity<?> deletesToSubList(@RequestBody RequestData data, HttpSession session) {
+        List<String> newValue = data.getNewValue();
+        int subListIndex = data.getSubListIndex();
+        log.info("add -> {}", newValue);
+        for (int i = 0; i < newValue.size(); i++) {
+            deleteToSubList(subListIndex, newValue.get(i), session);
+        }
+        return ResponseEntity.ok(newValue);
+    }
 
 
     @PostMapping("/his/add-to-sublist")
     @ResponseBody
-    public String addToSubList(@RequestParam int subListIndex, @RequestParam String newValue, HttpSession session) {
+    private String addToSubList(@RequestParam int subListIndex, @RequestParam String newValue, HttpSession session) {
         // 세션에서 이중 리스트 가져오기
         List<List<String>> nestedList = getSessionItems(session);
-        log.info("addToSubList--------> {}", subListIndex);
-        log.info("addToSubList--------> {}", newValue);
-        log.info(nestedList);
+
         if (nestedList == null) {
             return "No data found in session.";
         }
@@ -47,16 +73,14 @@ public class ChartSessionController {
 
     @PostMapping("/his/change-to-sublist")
     @ResponseBody
-    public String changeToSubList(@RequestParam int subListIndex, @RequestParam String newValue, HttpSession session) {
+    public String deleteToSubList(@RequestParam int subListIndex, @RequestParam String newValue, HttpSession session) {
         // 세션에서 이중 리스트 가져오기
         List<List<String>> nestedList = getSessionItems(session);
-        log.info("addToSubList--------> {}", subListIndex);
-        log.info("addToSubList--------> {}", newValue);
-        log.info(nestedList);
+
         if (nestedList == null) {
             return "No data found in session.";
         }
-
+        log.info("delete -> {}", newValue);
         // 원하는 subList가 존재하는지 확인하고 값 추가
         if (subListIndex < nestedList.size() && nestedList.get(subListIndex) != null) {
             List<String> innerList = nestedList.get(subListIndex); // subList[0] 배열 가져오기
@@ -73,8 +97,8 @@ public class ChartSessionController {
     // 세션에서 배열을 가져오는 엔드포인트
     @GetMapping("/his/get-session-items")
     public List<List<String>> getSessionItems(HttpSession session) {
-
-       List<List<String>> nestedList = (List<List<String>>) session.getAttribute("nestedList");
+        // 세션에서 "nestedList" 가져오기
+        List<List<String>> nestedList = (List<List<String>>) session.getAttribute("nestedList");
 
         // 세션에 값이 없을 경우 빈 이중 리스트를 반환
         if (nestedList == null) {
@@ -82,14 +106,13 @@ public class ChartSessionController {
             for (int i = 0; i < 4; i++) {
                 nestedList.add(new ArrayList<>());
                 if (i == 3) {
-                    nestedList.get(i).add("삭제");
+                    nestedList.get(i).add("0");
                 }
             }
-
-
         }
 
         log.info("sessionItem-> {}", nestedList);
         return nestedList;
     }
 }
+
