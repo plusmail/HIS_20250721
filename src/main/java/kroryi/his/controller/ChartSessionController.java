641a25ab97bcccdf82f6088bfa9daee9f31df8a0
Heoseongjune <tpdl7985@naver.com>
2024-10-02 15:37:07 +0900

    ajax 데이터 관리 일부 변경

--- src/main/java/kroryi/his/controller/ChartSessionController.java
+++ src/main/java/kroryi/his/controller/ChartSessionController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -17,30 +16,66 @@ import java.util.List;
 @RestController
 public class ChartSessionController {
 
+    List<List<List<String>>> piList = null;
+    List<List<String>> toothList = null;
+    List<List<String>> symptomList = null;
+    List<List<String>> memoList = null;
+    List<Integer> arrayList = null;
 
     @PostMapping(value = "/his/adds-to-sublist")
     private ResponseEntity<?> addsToSubList(@RequestBody RequestData data, HttpSession session) {
-        List<String> newValue = data.getNewValue();
+        List<String> newValues = data.getNewValues();
         int subListIndex = data.getSubListIndex();
-
         List<List<String>> nestedList = getSessionItems(session);
         List<String> innerList = nestedList.get(subListIndex);
-        for (int i = 0; i < newValue.size(); i++) {
-            boolean check = (innerList).contains(newValue.get(i));
+        for (int i = 0; i < newValues.size(); i++) {
+            boolean check = (innerList).contains(newValues.get(i));
             if (check) {
                 i++;
             } else {
-                addToSubList(subListIndex, newValue.get(i), session);
+                addToSubList(subListIndex, newValues.get(i), session);
             }
         }
-        return ResponseEntity.ok(newValue);
+        return ResponseEntity.ok(newValues);
+    }
+
+    @PostMapping("/his/addDelete-to-subList")
+    private ResponseEntity<?> addDeleteToSubList(@RequestBody RequestData data, HttpSession session) {
+        List<String> newValues = data.getNewValues();
+        int subListIndex = data.getSubListIndex();
+        boolean addDel = data.isAddOrDelete();
+        if(addDel) {
+            List<List<String>> nestedList = getSessionItems(session);
+            List<String> innerList = nestedList.get(subListIndex);
+            for (int i = 0; i < newValues.size(); i++) {
+                boolean check = (innerList).contains(newValues.get(i));
+                if (check) {
+                    i++;
+                } else {
+                    addToSubList(subListIndex, newValues.get(i), session);
+                }
+            }
+            return ResponseEntity.ok(newValues);
+        } else {
+
+            for (int i = 0; i < newValues.size(); i++) {
+                deleteToSubList(subListIndex, newValues.get(i), session);
+            }
+            return ResponseEntity.ok(newValues);
+        }
     }
 
     @PostMapping(value = "/his/deletes-to-sublist", consumes = "application/json")
     private ResponseEntity<?> deletesToSubList(@RequestBody RequestData data, HttpSession session) {
-        List<String> newValue = data.getNewValue();
+
+        log.info("requestData ------> {}", data.getNewValues());
+        log.info("requestData ------> {}", data.getSubListIndex());
+        log.info("requestData ------> {}", data.getListIndex());
+        log.info("requestData -----> {}",data.isAddOrDelete());
+        List<String> newValue = data.getNewValues();
         int subListIndex = data.getSubListIndex();
-        log.info("add -> {}", newValue);
+
+        log.info(data.getNewValues());
         for (int i = 0; i < newValue.size(); i++) {
             deleteToSubList(subListIndex, newValue.get(i), session);
         }
@@ -80,7 +115,6 @@ public class ChartSessionController {
         if (nestedList == null) {
             return "No data found in session.";
         }
-        log.info("delete -> {}", newValue);
         // 원하는 subList가 존재하는지 확인하고 값 추가
         if (subListIndex < nestedList.size() && nestedList.get(subListIndex) != null) {
             List<String> innerList = nestedList.get(subListIndex); // subList[0] 배열 가져오기
@@ -99,7 +133,6 @@ public class ChartSessionController {
     public List<List<String>> getSessionItems(HttpSession session) {
         // 세션에서 "nestedList" 가져오기
         List<List<String>> nestedList = (List<List<String>>) session.getAttribute("nestedList");
-
         // 세션에 값이 없을 경우 빈 이중 리스트를 반환
         if (nestedList == null) {
             nestedList = new ArrayList<>();
@@ -111,8 +144,48 @@ public class ChartSessionController {
             }
         }
 
-        log.info("sessionItem-> {}", nestedList);
+
         return nestedList;
     }
+
+    // 환자의 선택된 치아별 증상을 3중 배열에 저장. [[1,2,3치아의 상태], [5,6 치아의 상태},.....]
+    public List<?> piList(List<List<String>> nestedList, int arrayIndex) {
+        if (piList == null) {
+            piList = new ArrayList<>();
+        }
+        if (!piList.contains(nestedList)) {
+            piList.add(nestedList);
+        }
+
+
+        List<List<String>> piArray = piList.get(arrayIndex);
+
+
+        return piArray;
+    }
+
+    private ResponseEntity<?> dataArray(@RequestBody RequestData data) {
+        List<String> newValues = data.getNewValues();
+        int subListIndex = data.getSubListIndex();
+
+        if (toothList == null) {
+            toothList = new ArrayList<>();
+        }
+        ;
+        if (symptomList == null) {
+            symptomList = new ArrayList<>();
+        }
+        ;
+        if (memoList == null) {
+            memoList = new ArrayList<>();
+        }
+        if (arrayList == null) {
+            arrayList = new ArrayList<>();
+        }
+
+
+
+        return null;
+    }
+
 }
-//1111111
--- src/main/java/kroryi/his/dto/RequestData.java
+++ src/main/java/kroryi/his/dto/RequestData.java
@@ -3,11 +3,21 @@ package kroryi.his.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class RequestData{
-    private List<String> newValue;
+    private List<String> newValues;
     private int subListIndex;
+    private int listIndex;
+    //add일경우 true, delete일경우 false
+    boolean addOrDelete;
+
+    public void clear(){
+        this.newValues = new ArrayList<>();
+        this.subListIndex = 0;
+        this.listIndex = 0;
+    }
 }
